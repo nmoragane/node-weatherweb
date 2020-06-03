6974c35be77c20f37adfd342f524ef7bcc49cfc7
Nayomal Lakshitha <nmoragane@gmail.com>
2020-06-04 01:05:36 +0530

    onload func

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,16 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit', (e) => {
+window.onload= function (){
+    //Your entire JS code here
+    weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    
     const location = search.value
-
+    
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-
+    
     fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
@@ -23,7 +25,10 @@ weatherForm.addEventListener('submit', (e) => {
             }
         })
     })
-})
+    })
+}
+
+
 
 
 
